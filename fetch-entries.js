8147c96b68623e3173713dbb73d78825ac1779b1
Kennedy Rose <kennedyianrose@gmail.com>
2019-12-04 16:18:59 -0600

    Entry links

--- fetch-entries.js
+++ fetch-entries.js
@@ -1,5 +1,6 @@
 require(`dotenv`).config()
-const puppeteer = require('puppeteer')
+const puppeteer = require(`puppeteer`)
+const { outputJson } = require(`fs-extra`)
 
 const urls = []
 urls.length = 101
@@ -7,9 +8,11 @@ for(let i = urls.length; i--;){
 	urls[i] = `https://www.harmontown.com/category/podcasts/page/${i + 1}/`
 }
 
-async function login(){
+async function go(){
 	const browser = await puppeteer.launch({ headless: false })
 	const page = await browser.newPage()
+
+	// Login
 	await page.goto(`https://www.harmontown.com/wp-login.php`)
 	await page.focus(`#user_login`)
 	await page.keyboard.type(process.env.USERNAME)
@@ -17,7 +20,33 @@ async function login(){
 	await page.keyboard.type(process.env.PASSWORD)
 	await page.keyboard.press('Enter')
 
-	// await browser.close()
+	// Get entry links
+	const links = []
+	for (let i = urls.length; i--;) {
+		await page.goto(urls[i])
+		const foundLinks = await page.evaluate(() => {
+			const els = document.querySelectorAll(`a`)
+			const links = []
+			for (let i = els.length; i--;) {
+				const link = els[i].href
+				if (link && link.indexOf(`video-episode`) > -1) {
+					links.push(link)
+				}
+			}
+			return links
+		})
+		for(let i = foundLinks.length; i--;){
+			const link = foundLinks[i]
+			if (links.indexOf(link) === -1) {
+				links.push(link)
+			}
+		}
+	}
+
+	console.log(links)
+	await outputJson(`entries.json`, links)
+
+	await browser.close()
 }
 
-login()
+go()
