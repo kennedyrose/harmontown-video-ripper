d001d1ee7794cc4e715e754000d793921dc6004e
Kennedy Rose <kennedyianrose@gmail.com>
2019-12-04 17:28:23 -0600

    Save progress

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dl = require(`download-file-with-progressbar`)
 const { existsSync, mkdirSync, outputFile, readFile } = require(`fs-extra`)
 const links = require(`./video-links.json`)
 
-function asyncDl(url, key){
+function asyncDl(url){
 	let prog = 0
 	return new Promise((resolve, reject) => {
 		dl(url, {
@@ -29,17 +29,19 @@ function asyncDl(url, key){
 }
 
 !async function go(){
-	if (!existsSync(`./video`)) {
-		mkdirSync(`./video`)
+	if (!existsSync(`./videos`)) {
+		mkdirSync(`./videos`)
 	}
 	let start
 	try {
 		start = await readFile(`./.progress`)
+		start = Number(start)
 	}
 	catch(err){}
-	for(let i = start || 0; i < links.length; i++){
+	for (let i = start || 0; i < links.length; i++) {
+		await outputFile(`./.progress`, `${i}`)
 		const link = links[i]
 		console.log(`Downloading: ${link}`)
-		await asyncDl(link, i)
+		await asyncDl(link)
 	}
 }()
